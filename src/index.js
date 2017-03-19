7f8bedfa6d25961506086685b37b6d81124d3b92
Simon de Lang <simondelang@gmail.com>
2017-03-19 08:49:35 +0100

    fix(gitignore): Ignore js source files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var test_runner_1 = require('stryker-api/test_runner');
-var test_framework_1 = require('stryker-api/test_framework');
-var TapeTestRunner_1 = require('./TapeTestRunner');
-var TapeTestFramework_1 = require('./TapeTestFramework');
-test_runner_1.TestRunnerFactory.instance().register('tape', TapeTestRunner_1.default);
-test_framework_1.TestFrameworkFactory.instance().register('tape', TapeTestFramework_1.default);
-//# sourceMappingURL=index.js.map
